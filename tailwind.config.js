3a91b44758213c2fb3c5d5b7284c8f91ac40f2c8
IvaMatesic <iva.matesic1@gmail.com>
2024-08-24 11:43:59 +0200

    Add colors and fonts Add styling to header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#93B48B',
+          light: '#F7FFF6',
+          dark: '#8491A3'
+        },
+        secondary: {
+          DEFAULT: '#A5668B',
+          light: '#D3BCC0',
+          dark: '#6B2D5C',
+        },
+        accent: '#FF5376',  // Single custom color (no shades)
+      },
+    },
+    fontFamily: {
+      'title': ['Julius Sans One', 'sans-serif'],
+      'body': ['Inria Sans', 'sans-serif'],
+    },
   },
   plugins: [],
 }
