7c59222c0933a01ff4e9a972450079c0cfdd3c27
IvaMatesic <iva.matesic1@gmail.com>
2024-08-25 15:30:35 +0200

    Add navigation component with styles (#4)
    * Add navigation component with styles
    
    * Remove unnecessary code and imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './header/header.component';
 import {HomepageComponent} from './homepage/homepage.component';
+import {NavigationComponent} from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomepageComponent],
+  imports: [RouterOutlet, HeaderComponent, HomepageComponent, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {HomepageComponent} from './homepage/homepage.component';
+import {
+  BulkCreateWordTranslationsComponent
+} from './bulk-create-word-translations/bulk-create-word-translations.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomepageComponent },
+  { path: 'upload', component: BulkCreateWordTranslationsComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' },
+];
