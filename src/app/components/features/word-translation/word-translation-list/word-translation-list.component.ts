9df17aa131459a94772486d5d94f5f8996630f86
IvaMatesic <iva.matesic1@gmail.com>
2024-09-16 18:38:28 +0200

    Add shuffle words on repeat

--- src/app/components/features/word-translation/word-translation-list/word-translation-list.component.ts
+++ src/app/components/features/word-translation/word-translation-list/word-translation-list.component.ts
@@ -102,6 +102,15 @@ export class WordTranslationListComponent {
   }
 
   handleRepeatClick() {
-    this.setListsWithNewData(this.displayedList())
+    this.setListsWithNewData(this.shuffleArray(this.displayedList()))
+  }
+
+
+  shuffleArray(array: any[]): any[] {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
   }
 }
