5e0f5c8a99c7fa36d9c39a581f11cf369973c460
IvaMatesic <iva.matesic1@gmail.com>
2024-09-16 11:44:00 +0200

    Add repeat currently fetched translations

--- src/app/word-translation-list/word-translation-list.component.ts
+++ src/app/word-translation-list/word-translation-list.component.ts
@@ -33,7 +33,7 @@ export class WordTranslationListComponent {
   isLastWord = signal(false);
   areTranslationsFetched = signal(false);
   showSecondPart = signal(false);
-  numberOfLessons= signal(1);
+  numberOfLessons = signal(1);
   showFetchAgain = signal(false);
 
   protected readonly TranslationMode = TranslationMode;
@@ -43,17 +43,22 @@ export class WordTranslationListComponent {
 
   fetchTranslationsClicked() {
     this.wordTranslationService.getAllTranslations(this.selectedFetchMode(), this.numberOfLessons()).subscribe(response => {
-      this.translationList.set(response)
-      this.displayedList.set(this.translationList().slice(0, 1));
-      this.resetVariables();
+      this.setListsWithNewData(response);
     })
   }
 
-  resetVariables(){
+  setListsWithNewData(data: WordTranslation[]) {
+    this.translationList.set(data)
+    this.displayedList.set(this.translationList().slice(0, 1));
+    this.resetVariables();
+  }
+
+  resetVariables() {
     this.areTranslationsFetched.set(true);
     this.showFetchAgain.set(false);
     this.currentWordIndex.set(1);
     this.isLastWord.set(false);
+    this.showSecondPart.set(false);
   }
 
   @HostListener('window:keydown', ['$event'])
@@ -83,7 +88,7 @@ export class WordTranslationListComponent {
       this.showSecondPart.set(true);
   }
 
-  changeTranslationMode(mode: TranslationMode){
+  changeTranslationMode(mode: TranslationMode) {
     this.selectedTranslateMode.set(mode);
   }
 
@@ -95,5 +100,9 @@ export class WordTranslationListComponent {
   changeNumberOfLessons(numberOfLessons: number) {
     this.numberOfLessons.set(numberOfLessons);
     this.showFetchAgain.set(true);
+  }
+
+  handleRepeatClick() {
+    this.setListsWithNewData(this.displayedList())
   }
 }
